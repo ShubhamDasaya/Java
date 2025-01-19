5e8611ca94e88eec092b492b85524c36d3c48fb5
ShubhamDasaya <mrshubhsd@gmail.com>
2025-01-19 17:40:45 +0530

    Isosilece Triangle

---
+++ Program/IsosileceTriangle.java
@@ -0,0 +1,8 @@
+public class IsosileceTriangle {
+    public static void main(String[] args) {
+        int h = 15, b = 15;
+        double area;
+        area = b*h/2;
+        System.out.println("THE area will be :"+area);
+    }
+}
